98b506bd36dc60929dedf740eb505bb5912aa85a
Srinath <srinath.loyltwo3ks@gmail.com>
2024-01-04 10:28:45 +0530

    Commit 4(Some updates after revert commit)

--- app/src/main/java/com/srinath/gitcheck/MainActivity.kt
+++ app/src/main/java/com/srinath/gitcheck/MainActivity.kt
@@ -10,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.d("jkffldlfl","Commit 1")
+        Log.d("jkffldlfl","Some updates after revert commit")
     }
 }
