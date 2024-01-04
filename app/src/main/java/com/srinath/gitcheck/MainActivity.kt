5cb1bcd55efd664041f3b1427804f80d58fcc639
Srinath <srinath.loyltwo3ks@gmail.com>
2024-01-04 18:43:39 +0530

    Revert "Commit 3"
    This reverts commit 505da772f23ed42ba94c9ba33135490f4a86a3bc.

--- app/src/main/java/com/srinath/gitcheck/MainActivity.kt
+++ app/src/main/java/com/srinath/gitcheck/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("jkffldlfl","Commit 3")
+        Log.d("jkffldlfl","Commit 2")
     }
 }
